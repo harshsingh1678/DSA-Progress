7cfeca0e460815140f42b1ca83d0d183ae3b561f
harshsingh1678 <singhharsh88319@gmail.com>
2026-02-15 22:42:40 +0530

    Time: 0 ms (100%), Space: 47.2 MB (93.71%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,41 +2,44 @@ public class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
+
+        int[] row = new int[m];
+        int[] col = new int[n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    markRow(i, matrix);
-                    markCol(j, matrix);
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == -111) {
+                if (row[i] == 1 || col[j] == 1) {
                     matrix[i][j] = 0;
                 }
             }
         }
     }
 
-    private void markRow(int i, int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        for (int j = 0; j < n; j++) {
-            if (matrix[i][j] != 0) {
-                matrix[i][j] = -111;
-            }
-        }
-    }
+    // private void markRow(int i, int[][] matrix) {
+    //     int m = matrix.length;
+    //     int n = matrix[0].length;
+    //     for (int j = 0; j < n; j++) {
+    //         if (matrix[i][j] != 0) {
+    //             matrix[i][j] = -111;
+    //         }
+    //     }
+    // }
 
-    private void markCol(int j, int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        for (int i = 0; i < m; i++) {
-            if (matrix[i][j] != 0) {
-                matrix[i][j] = -111;
-            }
-        }
-    }
+    // private void markCol(int j, int[][] matrix) {
+    //     int m = matrix.length;
+    //     int n = matrix[0].length;
+    //     for (int i = 0; i < m; i++) {
+    //         if (matrix[i][j] != 0) {
+    //             matrix[i][j] = -111;
+    //         }
+    //     }
+    // }
 }
