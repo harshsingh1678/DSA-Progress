232a9c3b285f63fd62679f45006460b6e8efba62
harshsingh1678 <singhharsh88319@gmail.com>
2026-04-05 03:29:33 +0530

    Time: 0 ms (100%), Space: 47.2 MB (38.49%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
@@ -13,15 +13,33 @@
  *     }
  * }
  */
+// Striver sol
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        if(root == null) return new TreeNode (val);
+        if (root == null) {
+            root = new TreeNode(val);
+            return root;
+        }
 
-        if(root.val>val)
-        root.left = insertIntoBST(root.left, val);
-        else if(root.val<val)
-        root.right = insertIntoBST(root.right, val);
+        TreeNode cur = root; // TC - O(log n)
 
+        while (true) {
+            if (val <= cur.val) {
+                if (cur.left != null)
+                    cur = cur.left;
+                else {
+                    cur.left = new TreeNode(val);
+                    break;
+                }
+            } else {
+                if (cur.right != null)
+                    cur = cur.right;
+                else {
+                    cur.right = new TreeNode(val);
+                    break;
+                }
+            }
+        }
         return root;
     }
 }
