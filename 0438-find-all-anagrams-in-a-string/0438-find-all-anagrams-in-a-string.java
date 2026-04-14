f12e7ff1633d087d6b14dbbe34a2eb3e699ae36b
harshsingh1678 <singhharsh88319@gmail.com>
2026-04-15 03:26:50 +0530

    Time: 13 ms (48.12%), Space: 46.8 MB (60.84%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,43 +1,40 @@
+// using freq array instead hashmap (most optimal solution)
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         int i = 0;
         int j = 0;
         int k = p.length();
+        int count = k;
 
-        List<Integer> list = new ArrayList<>();
-        HashMap<Character, Integer> map = new HashMap<>();
+        List<Integer> ans = new ArrayList<>();
+        int[] freq = new int[26];
 
-        // Build frequency map
-        for (int m = 0; m < p.length(); m++) {
-            map.put(p.charAt(m), map.getOrDefault(p.charAt(m), 0) + 1);
+        // Build frequency array
+        // store freq of every element of p
+        for (int c : p.toCharArray()) {
+            freq[c - 'a']++;
         }
 
-        int count = map.size();
-
         while (j < s.length()) {
-            if (map.containsKey(s.charAt(j))) {
-                map.put(s.charAt(j), map.get(s.charAt(j)) - 1);
-                if (map.get(s.charAt(j)) == 0)
-                    count--;
-            }
+            if (freq[s.charAt(j) - 'a'] > 0)
+                count--;
+            freq[s.charAt(j) - 'a']--;
 
             if (j - i + 1 < k)
                 j++;
 
             else if (j - i + 1 == k) {
                 if (count == 0)
-                    list.add(i);
+                    ans.add(i);
 
-                if (map.containsKey(s.charAt(i))) {
-                    if (map.get(s.charAt(i)) == 0)
-                        count++;
+                if (freq[s.charAt(i) - 'a'] >= 0)
+                    count++;
 
-                    map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
-                }
+                freq[s.charAt(i) - 'a']++;
                 i++;
                 j++;
             }
         }
-        return list;
+        return ans;
     }
 }
