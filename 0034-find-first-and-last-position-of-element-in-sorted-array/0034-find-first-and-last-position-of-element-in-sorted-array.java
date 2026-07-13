79ee118d03b4a299fa0223fe89c3e2e11796123f
harshsingh1678 <singhharsh88319@gmail.com>
2026-07-13 14:48:46 +0530

    Time: 0 ms (100%), Space: 48 MB (71.05%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,44 +1,42 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        return new int[] { findFirst(nums, target), findLast(nums, target) };
+        return new int[] { firstElement(nums, target), lastElement(nums, target) };
     }
 
-    public static int findFirst(int[] arr, int x) {
+    public static int firstElement(int[] arr, int target) {
         int low = 0;
         int high = arr.length - 1;
         int first = -1;
-        int mid;
-
         while (low <= high) {
-            mid = low + (high - low) / 2;
-
-            if (x == arr[mid]) {
+            int mid = low + (high - low) / 2;
+            if (target == arr[mid]) {
                 first = mid;
                 high = mid - 1;
-            } else if (x < arr[mid])
+            } else if (target < arr[mid]) {
                 high = mid - 1;
-            else
+            } else {
                 low = mid + 1;
+            }
         }
         return first;
     }
 
-    public static int findLast(int[] arr, int x) {
+    public static int lastElement(int[] arr, int target) {
         int low = 0;
         int high = arr.length - 1;
         int last = -1;
-        int mid;
 
         while (low <= high) {
-            mid = low + (high - low) / 2;
+            int mid = low + (high - low) / 2;
 
-            if (x == arr[mid]) {
+            if (target == arr[mid]) {
                 last = mid;
                 low = mid + 1;
-            } else if (x < arr[mid])
+            } else if (target < arr[mid]) {
                 high = mid - 1;
-            else
+            } else {
                 low = mid + 1;
+            }
         }
         return last;
     }
